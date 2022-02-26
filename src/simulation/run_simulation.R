db5eac4eb2aa90e8f500a0663d53d9bec2fb9b06
the_weavs <natalieweaver720@gmail.com>
2022-02-26 00:22:32 -0800

    add run_simulation and helpers and update config keys

---
+++ src/simulation/run_simulation.R
@@ -0,0 +1,21 @@
+run_simulation <- function(config) {
+  
+  out_path <- file.path(config$out_dir, "results.csv")
+  
+  for (i in seq(config$n_iterations)) {
+    run_one_iteration(config, i, out_path, (i > 1))
+  }
+
+}
+
+run_one_iteration <- function(config, i, out_path, append) {
+  
+  out <- make_feature_matrix(config) %>%
+    assign_rows_to_groups(config) %>%
+    calculate_statistics(config) %>%
+    dplyr::mutate(iter = i, .before = group)
+  
+  readr::write_csv(out, out_path, append = append)
+  
+  out
+}
