c1edf452a7ac8615313767daca902724f7fb7934
the_weavs <natalieweaver720@gmail.com>
2022-02-25 23:26:14 -0800

    add calculate_statistics and helpers

---
+++ src/simulation/calculate_statistics.R
@@ -0,0 +1,22 @@
+calculate_statistics <- function(data, config) {
+  
+  data <- data %>%
+    tidyr::pivot_longer(
+      cols = 1:config$n_vars,
+      names_to = "feature",
+      values_to = "value"
+    )
+  
+  data <- data %>%
+    dplyr::group_by(group, feature) %>%
+    dplyr::summarize(
+      mean = mean(value)
+    ) %>%
+    dplyr::arrange(feature)
+  
+  data
+}
+
+get_statistics <- function(config) {
+  NULL
+}
