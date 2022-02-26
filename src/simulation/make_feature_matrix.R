cfc963059349d974ac9c8d3a6fdc26ed9ffe0f90
the_weavs <natalieweaver720@gmail.com>
2022-02-25 22:09:00 -0800

    add make_feature_matrix function and helpers

---
+++ src/simulation/make_feature_matrix.R
@@ -0,0 +1,33 @@
+make_feature_matrix <- function(config, to_tibble = TRUE) {
+  
+  n <- config$sample_size
+  p <- config$n_vars
+  
+  out <- matrix(
+    data = get_dist(config)(n * p),
+    nrow = n,
+    ncol = p
+  )
+  
+  if (to_tibble) {
+    out <- tibble::as_tibble(out, .name_repair = make_names)
+  }
+  
+  out
+}
+
+get_dist <- function(config) {
+  
+  if (config$feature_spec$dist == "binom") {
+    function(n) {
+      rbinom(n = n,
+             size = config$feature_spec$size,
+             prob = config$feature_spec$prob)
+    }
+  }
+  
+}
+
+make_names <- function(vec) {
+  paste("X", seq_along(vec), sep = "_")
+}
