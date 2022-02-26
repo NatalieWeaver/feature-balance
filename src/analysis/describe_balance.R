639dfd97db563b7da3815f411aebb98a3a6aea22
the_weavs <natalieweaver720@gmail.com>
2022-02-26 14:25:03 -0800

    add describe_iter_balance, describe_overall_balance functions and update config keys

---
+++ src/analysis/describe_balance.R
@@ -0,0 +1,33 @@
+describe_iter_balance <- function(summary, config) {
+  
+  iter_balance <- summary %>%
+    dplyr::summarize(
+      feature_balance_rate = mean(balanced)
+    ) %>%
+    dplyr::mutate(
+      meets_threshold = as.numeric(feature_balance_rate >= config$threshold)
+    )
+  
+  readr::write_csv(
+    iter_balance,
+    file.path(config$out_dir, "balance_by_iter.csv")
+  )
+  
+  iter_balance
+}
+
+describe_overall_balance <- function(iter_balance, config) {
+  
+  overall_balance <- iter_balance %>%
+    dplyr::summarize(
+      overall_feature_balance_rate = mean(feature_balance_rate),
+      pct_iters_meeting_threshold = mean(meets_threshold)
+    )
+  
+  readr::write_csv(
+    overall_balance,
+    file.path(config$out_dir, "overall_balance.csv")
+  )
+  
+  overall_balance
+}
