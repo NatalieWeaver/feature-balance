9c2fa19792874a6c3da7e8e8c8bec097894608f6
the_weavs <natalieweaver720@gmail.com>
2022-02-26 23:14:40 -0800

    add script for multi simulation runs

---
+++ multi.R
@@ -0,0 +1,9 @@
+# Run multiple simulations using specification from config
+
+library(pacman)
+pacman::p_load("yaml")
+
+source(file.path("src", "utilities.R"))
+
+config <- yaml::read_yaml("config.yaml")
+run_spec <- setup(config)
